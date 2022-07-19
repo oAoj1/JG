b9156b1429ac2e8c204277ea180c5be15529634d
oAoj1 <joaogbmarques@gmail.com>
2022-07-19 18:24:44 -0300

    adicionando pagina de HTML

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ abrir.onclick = function(){
     abrir.classList.toggle('active')
 }
 
-let html = document.querySelector('.abrirhtml')
+let html = document.querySelector('.open-html')
 html.onclick = function(){
     html.classList.toggle('active')
+}
+
+let css = document.querySelector('.css')
+css.onclick = function(){
+    css.classList.toggle('active')
+}
+
+let js = document.querySelector('.js')
+js.onclick = function(){
+    js.classList.toggle('active')
 }
